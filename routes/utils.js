141acbcd34247478c9b81869ab39233c5e2d55ce
karananeja <karananeja.r@gmail.com>
2023-07-02 22:30:56 +0530

    created utils route for generic apis

---
+++ routes/utils.js
@@ -0,0 +1,31 @@
+const router = require('express').Router();
+let UserEmail = require('../models/userEmailModel');
+const responseStructure = require('../utils/helpers');
+const countryList = require('../utils/constants');
+
+router.route('/country-list').get((req, res) => {
+  const accessToken = req.headers.authorization.split(' ')[1];
+
+  UserEmail.findOne({ accessToken: accessToken }).then((userFound) => {
+    if (!userFound || userFound === null) {
+      responseStructure({
+        res,
+        statusCode: 400,
+        data: {
+          err: 'USER_NOT_FOUND',
+          errMessage: `There's an issue with the token provided`,
+        },
+      });
+    } else {
+      responseStructure({
+        res,
+        data: {
+          countryList,
+          msg: 'Country List fetched successfully',
+        },
+      });
+    }
+  });
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -14,10 +14,12 @@ app.use(cors());
 // Importing the route files
 const usersSignUpRouter = require('./routes/signup');
 const usersSignInRouter = require('./routes/signin');
+const utils = require('./routes/utils');
 
 // API endpoints
 app.use('/api/v1', usersSignUpRouter);
 app.use('/api/v1', usersSignInRouter);
+app.use('/api/v1', utils);
 
 // Setting up the port and database connection url
 const port = process.env.NODE_APP_PORT || 4000;
