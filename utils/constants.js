08fa05270ae1bb8e2e6b5715bd60f9ce0db8810e
karananeja <karananeja.r@gmail.com>
2023-12-30 23:11:15 +0530

    moved the env values to constants in util file

--- utils/constants.js
+++ utils/constants.js
@@ -2,4 +2,10 @@ const countryList = [
   { countryCode: 'IND', dialCode: '+91', countryName: 'India' },
 ];
 
-module.exports = countryList;
+const environment = {
+  DB_PASSWORD: process.env.NODE_DB_PASSWORD,
+  DB_NAME: process.env.NODE_DB_NAME,
+  APP_PORT: process.env.NODE_APP_PORT,
+  JWT_SECRET: process.env.NODE_JWT_SECRET,
+  SECS_IN_ONE_DAY: 24 * 60 * 60,
+};
