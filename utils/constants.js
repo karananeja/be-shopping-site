0863d3fe5c2e07d8384da8bb662020a6a4bc2a34
karananeja <karananeja.r@gmail.com>
2023-12-30 23:13:03 +0530

    created common err msg obj

--- utils/constants.js
+++ utils/constants.js
@@ -10,4 +10,31 @@ const environment = {
   SECS_IN_ONE_DAY: 24 * 60 * 60,
 };
 
+const errMessages = {
+  EMAIL_ID_NOT_UNIQUE: {
+    err: 'EMAIL_ID_NOT_UNIQUE',
+    errMessage: 'This email address has already been registered.',
+  },
+  INVALID_FORMAT_EMAIL: {
+    err: 'INVALID_FORMAT_EMAIL',
+    errMessage: 'The email address provided is Invalid.',
+  },
+  USER_NOT_FOUND: {
+    err: 'USER_NOT_FOUND',
+    errMessage: `User has not registered or doesn't exists`,
+  },
+  INVALID_CREDENTIALS: {
+    err: 'INVALID_CREDENTIALS',
+    errMessage: 'Invalid credentials',
+  },
+  INTERNAL_SERVER_ERROR: {
+    err: 'INTERNAL_SERVER_ERROR',
+    errMessage: 'Exception has occurred',
+  },
+  INVALID_TOKEN: {
+    err: 'INVALID_TOKEN',
+    errMessage: `There's an issue with the token provided`,
+  },
+};
+
 const EMAIL_REGEX = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
