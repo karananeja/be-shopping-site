01ac2aecb27dfe59020b046c8c225a3d2810f58c
karananeja <karananeja.r@gmail.com>
2023-12-30 23:16:09 +0530

    updated the user info schema

--- models/userInfoModel.js
+++ models/userInfoModel.js
@@ -1,19 +1,16 @@
-const mongoose = require('mongoose');
-
-const Schema = mongoose.Schema;
+const { model, Schema } = require('mongoose');
 
 const userInfoSchema = new Schema(
   {
     email: { type: String, required: true },
     firstName: { type: String, trim: true },
     lastName: { type: String, trim: true },
-    email: { type: String, trim: true },
     phoneNumber: { type: String, trim: true },
     birthDate: { type: String, trim: true },
   },
   { timestamps: true }
 );
 
-const UserInfo = mongoose.model('userInfo', userInfoSchema);
+const UserInfo = model('userInfo', userInfoSchema);
 
 module.exports = UserInfo;
