4a2b49129cb4f6750cfa98a5a9c3ac03ecff04d0
karananeja <karananeja.r@gmail.com>
2025-03-31 16:21:32 +0530

    feat: create the address model

---
+++ models/addressModel.js
@@ -0,0 +1,25 @@
+const { model, Schema } = require('mongoose');
+const { PHONE_NO_REGEX } = require('../utils/constants');
+
+const userAddressSchema = new Schema(
+  {
+    type: { type: String, required: true },
+    name: { type: String, required: true },
+    phone: {
+      type: String,
+      required: true,
+      match: [PHONE_NO_REGEX, 'Please enter a valid 10 digit phone number'],
+    },
+    addressLine1: { type: String, required: true },
+    addressLine2: { type: String, required: true },
+    city: { type: String, required: true },
+    state: { type: String, required: true },
+    pincode: { type: Number, required: true, min: 100000, max: 999999 },
+    isDefault: { type: Boolean, default: false },
+  },
+  { timestamps: true }
+);
+
+const UserAddress = model('userAddress', userAddressSchema);
+
+module.exports = UserAddress;
